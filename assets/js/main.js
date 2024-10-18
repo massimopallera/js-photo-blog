31a6b68c95eb418a04428b821b35a97bc92173cd
massimopallera <pallera.massimo@gmail.com>
2024-10-18 11:33:13 +0200

    update: js prints cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,37 @@
-console.log('hi');
+// console.log('hi');
+
+
+const rowEl = document.querySelector('.row')
+
+const url = "https://jsonplaceholder.typicode.com/photos?_limit=6"
+
+axios.get(url)
+.then(response => {
+
+  const data = response.data
+
+  data.forEach(element => {
+
+    const {title, url} = element
+
+    const markup = `
+    <div class="my_col-3">
+      <div class="my_card p-4">
+
+        <img src="./assets/img/pin.svg" width="40" alt="" class="pin">
+        <img src="${url}" alt="" class="card-img">
+
+        <div class="card-body my-4">
+          ${title}
+        </div>
+
+      </div>
+    </div>
+    `
+    rowEl.innerHTML += markup
+
+    
+  });
+
+  console.log(data)
+})
